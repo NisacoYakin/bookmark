ef780b4373d2a3f6190022186675da2a886065ee
NisacoYakin <nisacoyakin@gmail.com>
2026-03-31 03:06:31 +0000

    Modication pour staging sur Render.com avec le Frontend fonctionnel avec CRUD complet

--- run.py
+++ run.py
@@ -1,15 +1,8 @@
-"""
-run.py
-Point d'entrée de l'application Bookmark.
-Lance le serveur Flask.
-"""
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    # Serveur de développement (pour Termux)
-    
-    port = int(os.environ.get("PORT", 5000))  # Render injecte PORT
-    app.run(host="0.0.0.0", port=port,   debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
